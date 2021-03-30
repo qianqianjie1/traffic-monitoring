d6651367fd539d7908c496a61662ad348cca0774
zlm06 <245573051@qq.com>
2021-03-30 21:57:43 +0800

    half-finished statistic

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import router from './router/index.js';
 import 'ant-design-vue/dist/antd.css';
 import Antd from 'ant-design-vue'
 import App from './App';
+import * as echarts from 'echarts';
 
 Vue.use(Antd)
 Vue.config.productionTip = false
@@ -12,3 +13,5 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.prototype.$echarts = echarts
